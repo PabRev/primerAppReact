218e148df8c39e043893cb873b8fd497f28b678f
PabloRev <pablorevello@live.com.ar>
2022-08-16 22:13:23 -0300

    Ejercicio NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+
+const NavBar = () => {
+    return(
+        <header> 
+            <a href="ecommerce.com">E-Commerce.com</a>
+            <ul>
+                <li>Inicio</li>
+                <li>Carrito</li>
+                <li>Promos</li>
+                <li>Atencion al cliente</li>
+            </ul>
+
+        </header>
+    )
+}
+
+export default NavBar
